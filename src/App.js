f6d41c7bcb90d9818e892810920bf2d9b2a7bdfd
Jaweriasaleem54 <125985442+Jaweriasaleem54@users.noreply.github.com>
2023-12-20 00:57:27 +0500

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavigationBar, { Navbar } from './Components/Navbar';
-import Crousal from './Components/Welcome';
-import SecurityUpdate from './Components/Securityupdate';
-import Statistics from './Components/Statistics';
 import Footer from './Components/Footer';
 import { Route, Routes } from 'react-router-dom';
-import Welcome from './Components/Welcome';
-import About from './Components/Contactus';
+import Maincomponent from './Components/Maincomponent';
 
 function App() {
   return (
     <>
-    <NavigationBar/>
-
-    <Routes>
-      
-    <Route path={'/'} element={<Welcome/>}/>
-<Route path={'Abt'} element={<About/>}/>
-
-</Routes>
-<Footer/>
+<Maincomponent/>
     </>
     );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Nav, Navbar, Button } from 'react-bootstrap';
-import logo from '../Assets/logo.png'; // Replace with the actual path to your logo
+import logo from '../Assets/image 3 (3).png'; // Replace with the actual path to your logo
 import { Link } from 'react-router-dom';
 
 const NavigationBar = () => {
@@ -22,10 +22,10 @@ const NavigationBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto me-5"> {/* Align items to the right with margin */}
-            <Nav.Link as={Link} to="/about" className="me-3">About</Nav.Link>
-            <Nav.Link as={Link} to="/contact" className="me-3">Contact us</Nav.Link>
-            <Nav.Link as={Link} to="/timetable" className="me-3">Timetable</Nav.Link>
-            <Nav.Link as={Link} to="/live-location" className="me-3">Live Location</Nav.Link>
+            <Nav.Link className="me-3">About</Nav.Link>
+            <Nav.Link className="me-3">Contact us</Nav.Link>
+            <Nav.Link className="me-3">Timetable</Nav.Link>
+            <Nav.Link className="me-3">Live Location</Nav.Link>
           </Nav>
           <Nav className="me-3"> {/* Add margin to the right side of the Nav containing the button */}
             <Button variant="danger">Sign In</Button>
